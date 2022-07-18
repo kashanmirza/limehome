adb029b527dbe460a627370c5e80a0daf891769d
kashan.baig <kashan.baig@10pearls.com>
2022-07-18 19:45:44 +0500

    miss spelling

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
+import * as dotenv from 'dotenv';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const port:number = +process.env.APP_PORT | 5000;
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidateInputPipe());
-  await app.listen(3002);
+  await app.listen(port);
 }
 bootstrap();
